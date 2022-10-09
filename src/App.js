437cb20a9c009a529f3c9a589c4b6669b4580cbe
Steven Zhang <steven@pursuit.org>
2022-10-09 15:38:34 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState } from "react";
+
 function App() {
+  const [myValue, setMyValue] = useState(0);
+  const [currentSetting, setCurrentSetting] = useState("normal");
+
+  function onButtonClick(number) {
+    // currentNumber = number;
+    setMyValue(number)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        { myValue }
+      </div>
+      <button onClick={() => onButtonClick(1)}>1</button>
+      <button onClick={() => onButtonClick(2)}>2</button>
+      <button onClick={() => onButtonClick(3)}>3</button>
+      <button onClick={() => onButtonClick(4)}>4</button>
+
+      <div>{ currentSetting }</div>
+      <button onClick={() => setCurrentSetting("normal")}>Normal</button>
+      <button onClick={() => setCurrentSetting("light")}>Light</button>
+      <button onClick={() => setCurrentSetting("dark")}>Dark</button>
     </div>
   );
 }
